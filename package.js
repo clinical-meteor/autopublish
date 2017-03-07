9ea6160c962b9b0d4a8e73e3d4a0a6159498720d
Abigail Watson <awatson1978@gmail.com>
2017-03-07 15:30:50 -0600

    initial commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'clinical:autopublish',
+  version: '1.0.0',
+  summary: 'Autopublish FHIR resources to logged in users (i.e. clinicians)',
+  git: 'https://github.com/clinical-meteor/autopublish',
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.3');
+
+  api.use('meteor-platform');
+  
+  api.addFiles('clinical-autopublish.js');
+});
