cf7f79f6a39e7cdfaf0fe4cb8e9aea011fcebe32
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-06-14 14:26:22 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:autopublish',
-  version: '1.0.26',
+  version: '1.0.27',
   summary: 'Autopublish FHIR resources to logged in users (i.e. clinicians)',
   git: 'https://github.com/clinical-meteor/autopublish',
   documentation: 'README.md'
